2728b04599aa9fe38d2795af7a94cad4367bc7b1
Charles Chin <Charles.Chin@bestbuy.com>
2017-08-04 16:13:57 -0700

    new JB4ASDK.framework with public headers included

---
+++ JB4ASDK.framework/Headers/JB4ASDK.h
@@ -0,0 +1,36 @@
+//
+//  JB4ASDK.h
+//  JB4ASDK
+//
+//  Created by Chin, Charles-CW on 8/3/17.
+//  Copyright © 2017 Bestbuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JB4ASDK.
+FOUNDATION_EXPORT double JB4ASDKVersionNumber;
+
+//! Project version string for JB4ASDK.
+FOUNDATION_EXPORT const unsigned char JB4ASDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JB4ASDK/PublicHeader.h>
+
+#import <JB4ASDK/ETAnalytics.h>
+#import <JB4ASDK/ETCloudPage.h>
+#import <JB4ASDK/ETCloudPageDataSource.h>
+#import <JB4ASDK/ETEventMessage.h>
+#import <JB4ASDK/ETEventRegion.h>
+#import <JB4ASDK/ETGenericUpdate.h>
+#import <JB4ASDK/ETLandingPagePresenter.h>
+#import <JB4ASDK/ETLocationManager.h>
+#import <JB4ASDK/ETMessage.h>
+#import <JB4ASDK/ETPush.h>
+#import <JB4ASDK/ETRegion.h>
+#import <JB4ASDK/ETWKLandingPagePresenter.h>
+#import <JB4ASDK/ExactTargetEnhancedPushDataSource.h>
+#import <JB4ASDK/PICart.h>
+#import <JB4ASDK/PICartItem.h>
+#import <JB4ASDK/PIOrder.h>
+#import <JB4ASDK/PushConstants.h>
+
