8fcfdf5b56d759a59eef99923266658237f6c982
Charles Chin <Charles.Chin@bestbuy.com>
2017-08-10 11:59:27 -0700

    removing all old frameworks and replace with the new FrameworkHub

---
+++ FrameworkHub.framework/Headers/FrameworkHub.h
@@ -0,0 +1,20 @@
+//
+//  FrameworkHub.h
+//  FrameworkHub
+//
+//  Created by Chin, Charles-CW on 8/9/17.
+//  Copyright © 2017 Bestbuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkHub.
+FOUNDATION_EXPORT double FrameworkHubVersionNumber;
+
+//! Project version string for FrameworkHub.
+FOUNDATION_EXPORT const unsigned char FrameworkHubVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkHub/PublicHeader.h>
+
+#import "AdobeMobileSDK/ADBMobile.h"
+#import "youtube-ios-player-helper/YTPlayerView.h"
