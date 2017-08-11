91def37355449c3ce2c595b95a1368d14f363399
Charles Chin <Charles.Chin@bestbuy.com>
2017-08-11 12:03:13 -0700

    updating the deployment target to 9.0

--- FrameworkHub.framework/Headers/FrameworkHub.h
+++ FrameworkHub.framework/Headers/FrameworkHub.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char FrameworkHubVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FrameworkHub/PublicHeader.h>
 
-#import "AdobeMobileSDK/ADBMobile.h"
+//#import "AdobeMobileSDK/ADBMobile.h"
